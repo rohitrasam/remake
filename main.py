bdb62ae792a11b3dc68103db5262663c06a46724
Rohit Rasam <rasamrohit@gmail.com>
2025-10-04 20:53:18 +0530

    enemy movement

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from typing import List
 import pygame as pg
 
+SCREEN_WIDTH, SCREEN_HEIGHT = 720, 720
+DISPLAY_WIDTH, DISPLAY_HEIGHT = 180, 180
 
 def animate(key: float, length: int, frame_speed: float) -> float:
     key += frame_speed
@@ -13,8 +15,11 @@ def get_rect(surf: pg.Surface, pos: pg.Vector2 | List[int]) -> pg.Rect:
     return surf.get_rect(center=pos)
 
 
-def enemy_movement() -> None:
-    pass
+def enemy_movement(enemy_pos: List[int], dir: int) -> None:
+
+    for idx in range(len(enemy_pos)):
+        enemy_pos[idx][0] += dir
+
 
 
 def player_movement(pos: pg.Vector2, delta: float, player: pg.Surface) -> None:
@@ -26,13 +31,13 @@ def player_movement(pos: pg.Vector2, delta: float, player: pg.Surface) -> None:
 
     if pos.x <= player.size[0] // 2:
         pos.x = player.size[0] // 2
-    if pos.x >= 180 - player.size[0] // 2:
-        pos.x = 180 - player.size[0] // 2
+    if pos.x >= DISPLAY_WIDTH - player.size[0] // 2:
+        pos.x = DISPLAY_WIDTH - player.size[0] // 2
 
 
 def main() -> None:
-    screen = pg.display.set_mode((720, 480))
-    display = pg.Surface((180, 120))
+    screen = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    display = pg.Surface((DISPLAY_WIDTH, DISPLAY_HEIGHT))
     clock = pg.Clock()
     running = True
     player: List[pg.Surface] = [
@@ -40,18 +45,19 @@ def main() -> None:
         pg.transform.rotate(pg.image.load("assets/images/player/2.png"), 90),
     ]
     player_key = 0
-    player_pos = pg.Vector2(90, 105)
+    player_pos = pg.Vector2(90, 165)
 
     enemy1: pg.Surface = pg.image.load("assets/images/enemy/1.png")
     enemy2: pg.Surface = pg.image.load("assets/images/enemy/2.png")
     enemy1_pos: List[List[int]] = [
         [(i * 16) + 10, 0] for i in range(11)
     ]  # [(x1, frame1)]
-    enemy1_y = 15
+    enemy_y = 20, 35
     enemy2_pos: List[List[int]] = [
         [(i * 16) + 10, 0] for i in range(11)
-    ]  # [(x1, frame1)]
-    enemy2_y = 31
+    ]
+    enemy_right= True
+    enemy_dir = 0.0
 
     defense: List[pg.Surface] = [
         pg.image.load("assets/images/defense/1.png"),
@@ -61,7 +67,7 @@ def main() -> None:
         pg.image.load("assets/images/defense/5.png"),
     ]
     defense_pos: List[List[int]] = [
-        [(i * 16) + 10, 80, 0] for i in range(11)
+        [(i * 16) + 10, 145, 0] for i in range(11)
     ]  # (x, y, frame)
     defense_key = 0
 
@@ -76,10 +82,10 @@ def main() -> None:
         player_key = animate(player_key, len(player), 0.1)
 
         for pos in enemy1_pos:
-            display.blit(enemy1, get_rect(enemy1, [pos[0], enemy1_y]))
+            display.blit(enemy1, get_rect(enemy1, [pos[0], enemy_y[0]]))
 
         for pos in enemy2_pos:
-            display.blit(enemy2, get_rect(enemy2, [pos[0], enemy2_y]))
+            display.blit(enemy2, get_rect(enemy2, [pos[0], enemy_y[1]]))
 
         for pos in defense_pos:
             display.blit(defense[defense_key], get_rect(defense[defense_key], pos[:2]))
@@ -87,7 +93,17 @@ def main() -> None:
         display.blit(
             player[int(player_key)], get_rect(player[int(player_key)], player_pos)
         )
-
+        
+        
+        if enemy1_pos[0][0] <= enemy1.size[0] // 2:
+            enemy_right = True
+        elif enemy1_pos[-1][0] >= DISPLAY_WIDTH - enemy1.size[0] // 2:
+            enemy_right = False
+        
+        enemy_dir = 0.02 if enemy_right else -0.02
+        
+        enemy_movement(enemy1_pos, enemy_dir)
+        enemy_movement(enemy2_pos, enemy_dir)
         player_movement(player_pos, delta, player[int(player_key)])
 
         screen.blit(pg.transform.scale(display, screen.size), (0, 0))
